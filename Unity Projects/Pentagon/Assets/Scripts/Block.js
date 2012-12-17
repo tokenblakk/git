54c3c566934899c9c19263d61d978c051f34af16
Tramel Jones <tokenblakk@gmail.com>
2012-12-16 22:20:57 -0600

    Tags and Prefabs
    New stuff

--- Unity Projects/Pentagon/Assets/Scripts/Block.js
+++ Unity Projects/Pentagon/Assets/Scripts/Block.js
@@ -1,3 +1,7 @@
+
+//var player: GameObject;
+//This block should have no logic for the player, just inherent qualities
+//handle all logic in the player class
 var symbol : String; 
 //try to give each shape via a string, and match strings within player object.
 var shapes  = new Array(
@@ -11,6 +15,6 @@ var shapes  = new Array(
 function Update()
 {
 	symbol = shapes[0];
-	print(symbol);
+	//print(symbol);
 }
 
